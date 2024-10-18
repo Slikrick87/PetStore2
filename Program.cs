95ab7a3fbf6fa326bbc022eb0bf8b415b56d2b53
Ricky Thomas <rjtom2@hotmail.com>
2024-10-18 18:56:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using PetStore;
+using System.Text.Json;
+
+Console.WriteLine("Press 1 to add a product.");
+Console.WriteLine("Type 'exit' to quit");
+string userInput = Console.ReadLine();
+
+while (userInput.ToLower() != "exit")
+{
+    Console.WriteLine("Press 1 to add a Product");
+    Console.WriteLine("Type 'exit' to quit");
+    userInput = Console.ReadLine();
+    if (userInput == "1")
+    {
+        DogLeash daRizzler = new DogLeash();
+        Console.Write("Name:");
+        daRizzler.Name = Console.ReadLine();
+        Console.Write("Price:");
+        daRizzler.Price = int.Parse(Console.ReadLine());
+        Console.Write("Quantity:");
+        daRizzler.Quantity = int.Parse(Console.ReadLine());
+        Console.Write("Description:");
+        daRizzler.Description = Console.ReadLine();
+        Console.Write("Length in inches:");
+        daRizzler.LengthInches = int.Parse(Console.ReadLine());
+        Console.Write("Material:");
+        daRizzler.Material = Console.ReadLine();
+        Console.WriteLine(JsonSerializer.Serialize(daRizzler));
+    }
+    else if (userInput == "exit")
+    {
+        Console.WriteLine("Exitting PetSTore. Press any key.");
+        Console.ReadLine();
+    }
+}
