d680799bb55a309c0b1f0a07571bba2bf7335e0b
Ricky Thomas <rjtom2@hotmail.com>
2024-10-20 15:40:49 -0400

    added code to hopefully construct new list(fingers crossed)

---
+++ ProductLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetStore;
+
+
+namespace PetStore
+{
+    internal class ProductLogic
+    {
+        List<Product> _products = new List<Product>();
+        
+            public ProductLogic()
+        {
+            _products = new List<Product>();
+        }
+        public void AddProduct()
+            { _products.Add(new Product()); }
+        }
+        
+    }
+
