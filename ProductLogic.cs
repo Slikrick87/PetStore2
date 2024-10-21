4224b1ee6a07370c011a294c138735a2ee57fc36
Ricky Thomas <rjtom2@hotmail.com>
2024-10-21 18:10:40 -0400

    commented out steps of next assignment since it's not working yet while i do more homework on methods.

--- ProductLogic.cs
+++ ProductLogic.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using PetStore;
 
-
-namespace PetStore
+namespace ProductLogic
 {
-    internal class ProductLogic
+    public class Logic
     {
-        List<Product> _products = new List<Product>();
+        private List<Product> _products = new List<Product>();
 
-        public ProductLogic()
+        public void ProductLogic()
         {
             _products = new List<Product>();
+
         }
-        public void AddProduct()
+        public void AddProduct(Product product)
         { _products.Add(new Product()); }
-
-
-        public List<Product> GetAllProducts()
+        public void GetAllProducts()
         {
-            return _products;
+            foreach (Product product in _products)
+            {
+                Console.WriteLine(_products);
+            }
         }
 
     }
-
 }
+
 
--- Program.cs
+++ Program.cs
@@ -3,37 +3,44 @@ using PetStore;
 using System;
 using System.ComponentModel.Design;
 using System.Text.Json;
+using ProductLogic;
 
 Console.WriteLine("Press 1 to add a product.");
 Console.WriteLine("Type 'exit' to quit");
-var productLogic = new ProductLogic();
+//var productLogic = new ProductLogic();
 string userInput = Console.ReadLine();
 
 while (userInput.ToLower() != "exit")
 {
     if (userInput == "1")
     {
-        DogLeash daRizzler = new DogLeash();
-        Console.Write("Name:");
-        daRizzler.Name = Console.ReadLine();
-        Console.Write("Price:");
-        daRizzler.Price = int.Parse(Console.ReadLine());
-        Console.Write("Quantity:");
-        daRizzler.Quantity = int.Parse(Console.ReadLine());
-        Console.Write("Description:");
-        daRizzler.Description = Console.ReadLine();
-        Console.Write("Length in inches:");
-        daRizzler.LengthInches = int.Parse(Console.ReadLine());
-        Console.Write("Material:");
-        daRizzler.Material = Console.ReadLine();
-        productLogic.AddProduct();
-        Console.WriteLine($"Product added: " + daRizzler.Name );
-        Console.WriteLine(JsonSerializer.Serialize(daRizzler));
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine("Press 1 to add another Product");
-        Console.WriteLine("Type exit to quit program");
-        //Console.WriteLine(daRizzler.Name);
-        userInput = Console.ReadLine();
+            DogLeash newDogleash = new DogLeash();
+            Console.Write("Name:");
+            newDogleash.Name = Console.ReadLine();
+            Console.Write("Price:");
+            newDogleash.Price = int.Parse(Console.ReadLine());
+            Console.Write("Quantity:");
+            newDogleash.Quantity = int.Parse(Console.ReadLine());
+            Console.Write("Description:");
+            newDogleash.Description = Console.ReadLine();
+            Console.Write("Length in inches:");
+            newDogleash.LengthInches = int.Parse(Console.ReadLine());
+            Console.Write("Material:");
+            newDogleash.Material = Console.ReadLine();
+           // productLogic.AddProduct();
+            Console.WriteLine($"Product added: " + newDogleash.Name);
+            Console.WriteLine(JsonSerializer.Serialize(newDogleash));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press 1 to add another Product");
+            Console.WriteLine("Type exit to quit program");
+            //Console.WriteLine(daRizzler.Name);
+            userInput = Console.ReadLine();
+        
+        }
+    else if (userInput == "8")
+    {
+        
+       // ProductLogic.GetAllProducts();
     }
-    }
+}
