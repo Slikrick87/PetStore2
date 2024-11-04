1aeeb496db5e051020a3bdf84c245006c2d48ee1
Ricky Thomas <rjtom2@hotmail.com>
2024-11-04 17:32:43 -0500

    added crude test code after failing to put into method. will reattempt and implementing constructors soon.

--- TestCode/TestCode.cs
+++ TestCode/TestCode.cs
@@ -9,76 +9,89 @@ namespace PetStore.TestCode
 {
     public class TestCode
     {
-        //public List<Product> _products = new List<Product>();
-        //public Dictionary<string, DogLeash> _DogLeash = new();
-        //public Dictionary<string, CatFood> _CatFood = new();
+        public List<Product> _products = new List<Product>();
+        public Dictionary<string, DogLeash> _DogLeash = new();
+        public Dictionary<string, CatFood> _CatFood = new();
 
-    //    public (Dictionary<string, Product>, List<Product>) TestCodeDict(Dictionary<string, Product> _DogLeash, Dictionary<string, Product> _CatFood)
-    //    {
-    //        var dogLeashClassTest = new DogLeashLogic();
-    //        var catFoodClassTest = Program.dogLeashClass;
-    //        DogLeash dogLeash1 = new DogLeash();
-    //        dogLeash1.Name = "Da Rizzler";
-    //        dogLeash1.Price = 14.99m;
-    //        dogLeash1.Quantity = 87;
-    //        dogLeash1.Description = "BadAss";
-    //        dogLeash1.LengthInches = 12;
-    //        dogLeash1.Material = "Leather";
-    //        dogLeashClassTest.AddDogLeash(dogLeash1);
-    //        //_products.Add(dogLeash1);
+        //public void ProductRepository()
+        //{
+        //    object TestRepositoryDogLeash = new DogLeashLogic();
+        //    object TestRepositoryCatFood = new CatFoodLogic();
+        //    object Dictionary<string, DogLeash> _DogLeash { get; set}
+        //        }
+
+        DogLeashLogic? TestRepositoryDogLeash { get; set; }
+        CatFoodLogic? TestRepositoryCatFood { get; set; }
+
+        //public void TestRepository(Dictionary<string, DogLeash> Dog_Leashes, Dictionary<string, CatFood> Cat_Food)
+        //{
+        //    //var DogLeashDict = new Dictionary<string, DogLeash>();
+        //    //var CatFoodDict = new Dictionary<string, CatFood>();
+        //    //var dogLeashClassTest = dogLeashClass as DogLeashLogic; 
+        //    //var catFoodClassTest = Program.dogLeashClass;
+        //    DogLeash dogLeash1 = new DogLeash();
+        //    dogLeash1.Name = "Da Rizzler";
+        //    dogLeash1.Price = 14.99m;
+        //    dogLeash1.Quantity = 87;
+        //    dogLeash1.Description = "BadAss";
+        //    dogLeash1.LengthInches = 12;
+        //    dogLeash1.Material = "Leather";
+        //    Dog_Leashes.Add(dogLeash1.Name, dogLeash1);
+        //    //_products.Add(dogLeash1);
 
 
-    //        DogLeash dogLeash2 = new();
-    //        dogLeash2.Name = "Lame AF";
-    //        dogLeash2.Price = 9.99m;
-    //        dogLeash2.Quantity = 20;
-    //        dogLeash2.Description = "Pretty Lame";
-    //        dogLeash2.LengthInches = 6;
-    //        dogLeash2.Material = "Some Lame Crap";
-    //        dogLeashClassTest.AddDogLeash(dogLeash2);
-    //        //_products.Add(dogLeash2);
+        //    DogLeash dogLeash2 = new();
+        //    dogLeash2.Name = "Lame AF";
+        //    dogLeash2.Price = 9.99m;
+        //    dogLeash2.Quantity = 20;
+        //    dogLeash2.Description = "Pretty Lame";
+        //    dogLeash2.LengthInches = 6;
+        //    dogLeash2.Material = "Some Lame Crap";
+        //    Dog_Leashes.Add(dogLeash2.Name, dogLeash2);
+        //    //_products.Add(dogLeash2);
 
-    //        CatFood catFood1 = new();
-    //        catFood1.Name = "Pringles";
-    //        catFood1.Price = 3.99m;
-    //        catFood1.Quantity = 24;
-    //        catFood1.Description = "Sour Cream and Onion";
-    //        catFood1.WeightPounds = 1;
-    //        catFood1.KittenFood = true;
-    //        catFoodClassTest.AddCatFood(catFood1);
-    //        //_products.Add(catFood1);
+        //    CatFood catFood1 = new();
+        //    catFood1.Name = "Pringles";
+        //    catFood1.Price = 3.99m;
+        //    catFood1.Quantity = 24;
+        //    catFood1.Description = "Sour Cream and Onion";
+        //    catFood1.WeightPounds = 1;
+        //    catFood1.KittenFood = true;
+        //    Cat_Food.Add(catFood1.Name, catFood1);
+        //    //_products.Add(catFood1);
 
-    //        CatFood catFood2 = new();
-    //        catFood2.Name = "Kittie Chow";
-    //        catFood2.Price = 19.99m;
-    //        catFood2.Quantity = 24;
-    //        catFood2.Description = "RoadKill";
-    //        catFood2.WeightPounds = 100;
-    //        catFood2.KittenFood = false;
-    //        catFoodClassTest.AddCatFood(catFood2);
-    //        //_products.Add(catFood2);
+        //    CatFood catFood2 = new();
+        //    catFood2.Name = "Kittie Chow";
+        //    catFood2.Price = 19.99m;
+        //    catFood2.Quantity = 24;
+        //    catFood2.Description = "RoadKill";
+        //    catFood2.WeightPounds = 100;
+        //    catFood2.KittenFood = false;
+        //    Cat_Food.Add(catFood2.Name, catFood2);
+        //    //_products.Add(catFood2);
 
-    //        CatFood catFood3 = new();
-    //        catFood3.Name = "Spam";
-    //        catFood3.Price = 5.99m;
-    //        catFood3.Quantity = 72;
-    //        catFood3.Description = "Shelf Stable!!";
-    //        catFood3.WeightPounds = 3;
-    //        catFood3.KittenFood = true;
-    //        catFoodClassTest.AddCatFood(catFood3);
-    //        //_products.Add(catFood3);
+        //    CatFood catFood3 = new();
+        //    catFood3.Name = "Spam";
+        //    catFood3.Price = 5.99m;
+        //    catFood3.Quantity = 72;
+        //    catFood3.Description = "Shelf Stable!!";
+        //    catFood3.WeightPounds = 3;
+        //    catFood3.KittenFood = true;
+        //    Cat_Food.Add(catFood3.Name, catFood3);
+        //    //_products.Add(catFood3);
 
-    //        DogLeash dogLeash3 = new();
-    //        dogLeash3.Name = "The One Ring";
-    //        dogLeash3.Price = 199.99m;
-    //        dogLeash3.Quantity = 20;
-    //        dogLeash3.Description = "Pretty Powerful";
-    //        dogLeash3.LengthInches = 6;
-    //        dogLeash3.Material = "Powerful Magic";
-    //        dogLeashClassTest.AddDogLeash(dogLeash3);
-    //        //_products.Add(dogLeash3);
-    //    }
+        //    DogLeash dogLeash3 = new();
+        //    dogLeash3.Name = "The One Ring";
+        //    dogLeash3.Price = 199.99m;
+        //    dogLeash3.Quantity = 20;
+        //    dogLeash3.Description = "Pretty Powerful";
+        //    dogLeash3.LengthInches = 6;
+        //    dogLeash3.Material = "Powerful Magic";
+        //    Dog_Leashes.Add(dogLeash3.Name, dogLeash3);
+        //    //_products.Add(dogLeash3);
 
+        //    //return CatFoodDict;
+        //}
     }
 }
 
