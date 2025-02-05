9129c5416a9abc0e18a95196ccef1ab9e6e0f28f
Ricky Thomas <rjtom2@hotmail.com>
2025-02-05 16:33:07 -0500

    started tests realized after I made OrderRepo class then moved all of those methods into ProductRepo class so it will work like I wanted that I was supposed to implement both classes. so I'm saving here so i don't redo everything three times again.

---
+++ ProductLogicTests/ProductLogicTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using PetStore;
+using PetStore.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProductLogicTests
+{
+    [TestClass]
+    public sealed class ProductLogicTests
+    {
+        [TestMethod]
+        public void GetProductById_CallsRepo()
+        {
+            //Arrange
+            Mock<IProductRepository> repository = new Mock<IProductRepository>();
+            Mock<IOrderRepository> orderRepository = new Mock<IOrderRepository>();
+            // Act
+
+            //Assert
+        }
+    }
+}
